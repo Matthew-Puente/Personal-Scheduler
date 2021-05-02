bb2d580eb444feb397db239f47eac8078719f862
kscrogginsjr <kscrogginsjr@gmail.com>
2021-05-01 19:48:02 -0700

    Basic date class

---
+++ Date.java
@@ -0,0 +1,20 @@
+
+public class Date {
+	
+	public static int month;
+	public static int day;
+	
+	
+	
+	public int getMonth()
+	{
+		return month;
+		
+	}
+	
+	public int getDay()
+	{
+		return day;
+	}
+
+}
