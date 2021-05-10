ba7dea5877c8b9ac749980471093b121edda27bd
Vincent Verdugo <38062516+vdugo@users.noreply.github.com>
2021-05-10 14:35:27 -0700

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,3 +1,5 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 public class Driver
@@ -11,9 +13,17 @@ public class Driver
 
 		Scanner scanner = new Scanner(System.in);
 
-		String input = "";
+		boolean creationSuccessful = false;
+
+		java.util.Date date;
+
+		String name, type, frequency;
+
+		String dateString = "";
 
 		char choice = 'n';
+
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM-dd-yyyy");
 
 		System.out.println("Would you like to import tasks from a file? (y/n)");
 		choice = scanner.next().charAt(0);
@@ -23,11 +33,11 @@ public class Driver
 			// open the users file
 		}
 
-		Date tempStartDate = new Date(3,14); // March 14th
+		Date tempStartDate = new Date(01,01);
 
-		Date tempEndDate = new Date(7,14);
+		Date tempEndDate = new Date(01,01);
 
-		Date dateToRemove = new Date(5, 14);
+		Date dateToRemove = new Date(01, 01);
 
 		while (choice != '5')
 		{
@@ -38,7 +48,10 @@ public class Driver
 			System.out.println("5. quit");
 
 			choice = scanner.next().charAt(0);
-
+			scanner.nextLine();
+			// need way to convert from real time to 15 min increments, each time interval is for the real day
+			// tempStartDate and tempEndDate is the time interval for a recursive task from one day to another
+			// dateToRemove is the day to cancel out the given startTime and endTime for to add the anti task
 			switch (choice)
 			{
 				case '1':
@@ -47,23 +60,149 @@ public class Driver
 					break;
 
 				case '2':
-					TransientTask transientTask = new TransientTask(tempStartDate,720, 1080, "Working on Projects", "Study");
+					System.out.println("Enter the date for a transient task in the format: MM-dd-yyyy");
+					dateString = scanner.nextLine();
 
-					boolean wow = scheduler.createTransientTask(transientTask);
+					try
+					{
+						date = simpleDateFormat.parse(dateString);
+						System.out.println("Given time in ms (unix time): " + date.getTime());
+
+					}
+					catch(ParseException e)
+					{
+						e.printStackTrace();
+					}
+
+					/*
+					take substrings of the user's dateString to get month and day
+					.substring method takes arguments (startIndex, endIndex + 1)
+					 */
+					tempStartDate.setMonth(Integer.parseInt(dateString.substring(0,2)));
+					tempStartDate.setDay(Integer.parseInt(dateString.substring(3,5)));
+
+					System.out.println("Enter the name of the transient task:");
+					name = scanner.nextLine();
+
+					System.out.println("Enter the type of the transient task:");
+					type = scanner.nextLine();
+
+					TransientTask transientTask = new TransientTask(tempStartDate,720, 1080, name, type);
+
+					creationSuccessful = scheduler.createTransientTask(transientTask);
+
+					if (creationSuccessful)
+					{
+						System.out.println("Successfully created transient task");
+					}
+					else
+					{
+						System.out.println("Failed to create transient task");
+					}
 
 					break;
 
 				case '3':
-					RecursiveTask recursiveTask = new RecursiveTask("M", "Napping", "Survival", tempStartDate,tempEndDate, 360, 420);
+					System.out.println("Enter the start date for a recursive task in the format: MM-dd-yyyy");
+					dateString = scanner.nextLine();
 
-					wow = scheduler.createRecursiveTask(recursiveTask);
+					try
+					{
+						date = simpleDateFormat.parse(dateString);
+						System.out.println("Given time in ms (unix time): " + date.getTime());
+
+					}
+					catch(ParseException e)
+					{
+						e.printStackTrace();
+					}
+
+					/*
+					take substrings of the user's dateString to get month and day
+					.substring method takes arguments (startIndex, endIndex + 1)
+					 */
+					tempStartDate.setMonth(Integer.parseInt(dateString.substring(0,2)));
+					tempStartDate.setDay(Integer.parseInt(dateString.substring(3,5)));
+
+					System.out.println("Enter the end date for a recursive task in the format: MM-dd-yyyy");
+					dateString = scanner.nextLine();
+
+					try
+					{
+						date = simpleDateFormat.parse(dateString);
+						System.out.println("Given time in ms (unix time): " + date.getTime());
+
+					}
+					catch(ParseException e)
+					{
+						e.printStackTrace();
+					}
+
+					/*
+					take substrings of the user's dateString to get month and day
+					.substring method takes arguments (startIndex, endIndex + 1)
+					 */
+					tempEndDate.setMonth(Integer.parseInt(dateString.substring(0,2)));
+					tempEndDate.setDay(Integer.parseInt(dateString.substring(3,5)));
+
+					System.out.println("Enter the frequency of the transient task (D for daily, W for weekly, M for monthly, CAPS SENSITIVE:");
+					frequency = scanner.nextLine();
+
+					System.out.println("Enter the name of the transient task:");
+					name = scanner.nextLine();
+
+					System.out.println("Enter the type of the transient task:");
+					type = scanner.nextLine();
+
+					RecursiveTask recursiveTask = new RecursiveTask(frequency, name, type, tempStartDate,tempEndDate, 360, 420);
+
+					creationSuccessful = scheduler.createRecursiveTask(recursiveTask);
+
+					if (creationSuccessful)
+					{
+						System.out.println("Successfully created recursive task");
+					}
+					else
+					{
+						System.out.println("Failed to create recursive task");
+					}
 
 					break;
 
 				case '4':
-					AntiTask antiTask = new AntiTask(dateToRemove,360,420);
+					System.out.println("Enter the date for an anti task in the format: MM-dd-yyyy");
+					dateString = scanner.nextLine();
 
-					wow = scheduler.removeTask(antiTask);
+					try
+					{
+						date = simpleDateFormat.parse(dateString);
+						System.out.println("Given time in ms (unix time): " + date.getTime());
+
+					}
+					catch(ParseException e)
+					{
+						e.printStackTrace();
+					}
+
+					/*
+					take substrings of the user's dateString to get month and day
+					.substring method takes arguments (startIndex, endIndex + 1)
+					 */
+					dateToRemove.setMonth(Integer.parseInt(dateString.substring(0,2)));
+					dateToRemove.setDay(Integer.parseInt(dateString.substring(3,5)));
+
+					AntiTask antiTask = new AntiTask(dateToRemove,720,1080);
+
+					creationSuccessful = scheduler.removeTask(antiTask);
+
+					if (creationSuccessful)
+					{
+						System.out.println("Successfully created anti task");
+					}
+					else
+					{
+						System.out.println("Failed to create anti task");
+					}
 
 					break;
 
@@ -78,5 +217,4 @@ public class Driver
 	}
 
 }
-
 
