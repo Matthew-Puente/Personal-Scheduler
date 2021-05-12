816385b4ec5e619b0774b222e7c928560e6aed24
kscrogginsjr <kscrogginsjr@gmail.com>
2021-05-11 20:54:50 -0700

    Added Year Feature

--- Calendar.java
+++ Calendar.java
@@ -1,15 +1,13 @@
-package basePackage;
-
 import java.util.HashMap;
 
 public class Calendar {
 	
-	private static HashMap<Integer, HashMap<Integer,int[]>> calendar;
+	private static HashMap<Integer, HashMap<Integer, HashMap<Integer,int[]>>> PSS;
 	private static final int MONTHS = 12;
 	
 	public Calendar()
 	{
-		calendar = new HashMap<>();
+		PSS = new HashMap<>();
 		generateCalendar();
 	}
 	
@@ -21,43 +19,53 @@ public class Calendar {
 	{
 		
 		int[] daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		
+		
+		// Initializes our PSS with a 2021 calendar
+		PSS.put(2021, new HashMap<>());
 	
 		for(int i=0; i< MONTHS; i++)
 		{			
-			calendar.put(i+1, new HashMap<Integer, int[]>()); // Fills HashMap with month numbers and HashMap signifying days.
-		}
-		
-		
-		//This loop handles the inner hashMap which maps a day in a month to an array
-		for(int i=0; i<daysInMonth.length; i++)
-		{
+			// Fills the Year 2021 with 12 "months"
+			PSS.get(2021).put(i+1, new HashMap<Integer, int[]>());
+			
 			for(int j=1; j<=daysInMonth[i]; j++)
 			{
-				calendar.get(i+1).put((Integer)j, new int[96]);
+				PSS.get(2021).get(i+1).put((Integer)j, new int[96]);
 				
 			}
-			
-		}
-		
+		}	
 	}
+		
 	
 	
 	//Returns array for specified day
 	public int[] getDay(Date date)
 	{
-		return calendar.get(date.getMonth()).get(date.getDay());
+		return PSS.get(date.getYear()).get(date.getMonth()).get(date.getDay());
+	}
+	
+	//Returns true if year is present;
+	public boolean containsYear(Date date)
+	{
+		return PSS.containsKey(date.getYear());
 	}
 	
 	
 	//Updates the day array
 	public boolean updateDay(Date date, int[] arr)
 	{
-		if(calendar.containsKey(date.getMonth()) && calendar.get(date.getMonth()).containsKey(date.getDay()))
+		if(PSS.containsKey(date.getYear()))
 		{
-			calendar.get(date.getMonth()).put(date.getDay(), arr);
-			return true;
+			if(PSS.get(date.getYear()).containsKey(date.getMonth()) && PSS.get(date.getYear()).get(date.getMonth()).containsKey(date.getDay()))
+			{
+				PSS.get(date.getYear()).get(date.getMonth()).put(date.getDay(), arr);
+				return true;
+				
+			}
 			
 		}
+		
 		
 		return false;	
 	}
