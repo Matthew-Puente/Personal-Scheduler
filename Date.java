dece4eee277a52d18f07b80cd99c39c8cce70713
kscrogginsjr <kscrogginsjr@gmail.com>
2021-05-11 20:55:59 -0700

    Added year attribute w/ getter/setter methods

--- Date.java
+++ Date.java
@@ -1,33 +1,48 @@
-package basePackage;
+
 
 public class Date {
 	
-	private int month;
 	private int day;
+	private int month;
+	private int year;
 	
 	public Date(int month, int day)
 	{
 		this.month = month;
 		this.day = day;
 	}
-	public void setMonth(int month)
-	{
-		this.month = month;
-	}
+	
 	public void setDay(int day)
 	{
 		this.day =day;
 	}
-	public int getMonth()
+	
+	public void setMonth(int month)
 	{
-		return month;
-		
+		this.month = month;
+	}
+	
+	public void setYear(int year)
+	{
+		this.year = year;
 	}
 	
 	public int getDay()
 	{
 		return day;
 	}
+	
+	public int getMonth()
+	{
+		return month;	
+	}
+	
+	public int getYear()
+	{
+		return year;
+	}
+	
+	
 	
 	public boolean equals(Date comparedDate)
 	{
